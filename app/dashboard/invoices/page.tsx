70ae8cddb872ddde0597c26f4cbb92d0d596f2ad
haozes <haozes@gmail.com>
2024-05-23 11:35:59 +0800

    update

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,6 +8,12 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+};
+
 export default async function Page({
     searchParams,
   }: {
